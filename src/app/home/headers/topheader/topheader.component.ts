8fed0171d3e378f015edc6c7a583db42c800077b
ibrahimmujhid <ijmujhid@hawaii.edu>
2024-03-22 17:19:05 -1000

    first commit

---
+++ src/app/home/headers/topheader/topheader.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-topheader',
+  standalone: true,
+  imports: [],
+  templateUrl: './topheader.component.html',
+})
+export class TopheaderComponent {
+
+}
